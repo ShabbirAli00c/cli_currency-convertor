44b11896bf1cb98cc61ccccd1e00a5f0c8ac96aa
ShabbirAli00c <159994827+ShabbirAli00c@users.noreply.github.com>
2024-04-09 11:24:59 +0500

    cli_currency-converto

---
+++ index.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const currency = {
+    USD: 1,
+    EUR: 0.92,
+    GBP: 0.79,
+    INR: 83.24,
+    PKR: 277,
+    JOD: 0.71,
+    BHD: 0.38
+};
+let user_answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter from yuor Currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'JOD', 'BHD', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "to",
+        message: "Enter to yuor Currency",
+        type: "list",
+        choices: ['USD', 'EUR', 'JOD', 'BHD', 'GBP', 'INR', 'PKR']
+    },
+    {
+        name: "amount",
+        message: "Enter your Amount",
+        type: "number",
+    }
+]);
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
+let amount = user_answer.amount;
+let baseAmont = amount / fromAmount;
+let convertedAmount = baseAmont * toAmount;
+console.log(Math.round(convertedAmount));
